bc6f4f85e288f1f145196f215a0d5e54daf51182
stephen <94320530+stephen-kern@users.noreply.github.com>
2022-10-05 11:09:20 -0500

    project set up and wrote the schema tables

---
+++ db/schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS employees;
+
+CREATE DATABASE employees;
+
+USE employees;
+
+CREATE TABLE departments (
+    department_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(30) UNIQUE NOT NULL
+);
+
+CREATE TABLE roles (
+    role_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10,2) NOT NULL, 
+    department_id INTEGER NOT NULL,
+    CONSTRAINT fk_departments FOREIGN KEY (department_id) REFERENCES departments(department_id) ON DELETE CASCADE
+);
+
+CREATE TABLE employees (
+    employee_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL, 
+    manager_id INTEGER UNSIGNED, 
+    CONSTRAINT fk_roles FOREIGN KEY (role_id) REFERENCES roles(role_id) ON DELETE CASCADE,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employees(employee_id) ON DELETE SET NULL
+);
